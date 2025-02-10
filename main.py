94c04fe840516f4a2e9093567bcad0779f850467
Marileni Sinioraki <94244395+Marileni@users.noreply.github.com>
2025-02-10 19:16:27 +0200

    Create main.py

---
+++ main.py
@@ -0,0 +1,19 @@
+import argparse
+from domain_adaptation.run_domain_adaptation import run_domain
+from multilingual.run_multilingual import run_multilingual
+from ner_preprocessing.run_ner_preprocessing import run_ner
+
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--approach', choices=['domain', 'multilingual', 'ner'], required=True)
+    args = parser.parse_args()
+
+    if args.approach == 'domain':
+        run_domain()
+    elif args.approach == 'multilingual':
+        run_multilingual()
+    elif args.approach == 'ner':
+        run_ner()
+
+if __name__ == "__main__":
+    main()

1d665f077f0e11d9e127445c7fac5d3972327d3e
Marileni Sinioraki <94244395+Marileni@users.noreply.github.com>
2025-02-11 13:16:46 +0200

    Update run_ner_preprocessing.py

--- ner_preprocessing/run_ner_preprocessing.py
+++ ner_preprocessing/run_ner_preprocessing.py
@@ -1,6 +1,6 @@
 from .data_processing import preprocess_data
 
-def run_domain():
+def run_ner():
     # 1) Load and preprocess data
     data = preprocess_data()
     # 2) Evaluate, output results, etc. 

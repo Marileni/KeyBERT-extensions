9d3f84acc9098b69e26c422b24bb524c3803077d
Marileni Sinioraki <94244395+Marileni@users.noreply.github.com>
2025-02-11 15:06:03 +0200

    Update run_multilingual.py

--- multilingual/run_multilingual.py
+++ multilingual/run_multilingual.py
@@ -12,8 +12,18 @@ def run_multilingual():
     dev_examples   = data_splits["dev"]
     test_examples  = data_splits["test"]
     
-    # 3) Train the model on train_examples
-    model = train_model(train_examples, dev_examples)
+    # 3) Fine-tune the model on train_examples/dev_examples
+    # (You can tune any parameters, such as epochs, LR, output path, etc.)
+    model = train_model(
+        train_examples,
+        dev_examples,
+        model_name="sentence-transformers/paraphrase-multilingual-MiniLM-L12-v2",
+        output_path="my_bi_encoder_model",
+        num_epochs=4,
+        batch_size=8,
+        warmup_steps=100,
+        learning_rate=3e-5
+    )
     
     # 4) Evaluate, output results, etc.
     

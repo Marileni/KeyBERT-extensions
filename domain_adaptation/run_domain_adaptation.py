a69453f3b3c9db4f61c272ce7fbf1dd417d9d68f
GinevraGaleotti <ginevra.galeotti@virgilio.it>
2025-02-14 11:32:25 +0100

    run_domain_adaptation.py

--- domain_adaptation/run_domain_adaptation.py
+++ domain_adaptation/run_domain_adaptation.py
@@ -1,12 +1,6 @@
 from .data_processing import preprocess_data
 from .model_training import train_model
 
-def run_domain():
-    # 1) Load and preprocess data
-    data = preprocess_data()
-    # 2) Train or fine-tune your model
-    model = train_model(data)
-    # 3) Evaluate, output results, etc.
     
 from .data_processing import preprocess_data
 from .model_training import train_model
